1ea4f780ea44ccda3efc2c7960e9a41807164470
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2021-08-17 21:00:29 -0300

    Aqui temos o problema do diamante. Na tentativa de implementar a classe ComboDevice.

---
+++ ProblemaDoDiamante/Devices/ComboDevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemaDoDiamante.Devices
+{
+    /* Veja que neste caso não posso fazer a classe ComboDevice herdar das
+     * classes Printer e Scanner, justamente pelo problema do diamente.
+     */
+    class ComboDevice : Printer, Scanner
+    {
+    }
+}
